59308b4942f68c9ad774c8ae828d41da3294ad39
g.veschi <g.veschi@thalia.de>
2021-08-31 12:53:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/modularizedapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.modularizedapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.modularizedapp.ui.theme.ModularizedAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ModularizedAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ModularizedAppTheme {
+        Greeting("Android")
+    }
+}
